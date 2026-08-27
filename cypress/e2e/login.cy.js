7d0578446e001ca7371a9aa019f0d79e6d3cdd26
pierobeat <johnpierrehaumahu@gmail.com>
2026-08-27 17:08:00 +0700

    update login & register pages ui

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,13 @@
 describe('Login flow', () => {
-  it('should login successfully', () => {
+  it('should display homepage after successful login', () => {
     cy.visit('/login');
 
     cy.get('input[placeholder="Email"]').type('testingdummy@mail.com');
 
     cy.get('input[placeholder="Password"]').type('test1234');
 
-    cy.contains('button', 'Login').click();
+    cy.contains('button', /^Login$/).click();
 
-    cy.url().should('not.include', '/login');
+    cy.get('header').should('be.visible');
   });
 });
