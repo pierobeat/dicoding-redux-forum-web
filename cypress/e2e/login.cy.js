db1023bbca827c556afeb30d5ef4407d117ac810
pierobeat <johnpierrehaumahu@gmail.com>
2026-08-27 19:07:34 +0700

    fixing cypress failed

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 describe('Login flow', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5173/login');
+    cy.visit('/login');
   });
 
   it('should display login page correctly', () => {
@@ -12,20 +12,29 @@ describe('Login flow', () => {
   });
 
   it('should login successfully', () => {
-    cy.intercept('POST', 'https://forum-api.dicoding.dev/v1/login').as(
-      'loginRequest',
-    );
+    cy.intercept('POST', '**/login', {
+      statusCode: 200,
+      body: {
+        status: 'success',
+        data: { token: 'dummy_token' },
+      },
+    }).as('loginRequest');
+
+    cy.intercept('GET', '**/users/me', {
+      statusCode: 200,
+      body: {
+        status: 'success',
+        data: { user: { id: 'user-1', name: 'testingdummy' } },
+      },
+    });
 
     cy.get('input[placeholder="Email"]').type('testingdummy@mail.com');
-
     cy.get('input[placeholder="Password"]').type('test1234');
 
     cy.contains('button', /^Login$/).click();
 
-    cy.wait('@loginRequest').then((interception) => {
-      console.log('LOGIN RESPONSE:', interception.response);
-    });
+    cy.wait('@loginRequest');
 
-    cy.contains('home-page', { timeout: 10000 }).should('be.visible');
+    cy.get('.home-page', { timeout: 10000 }).should('be.visible');
   });
 });
